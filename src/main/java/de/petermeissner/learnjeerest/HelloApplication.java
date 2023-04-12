063ff557577d5d8909e60250bf308b901f612bbf
Peter Meißner <retep.meissner@gmail.com>
2023-04-12 17:40:37 +0200

    init

---
+++ src/main/java/de/petermeissner/learnjeerest/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.petermeissner.learnjeerest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends javax.ws.rs.core.Application {
+
+}
