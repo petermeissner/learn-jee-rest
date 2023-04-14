0e42455d973931e900c40992da4a9d9b824ac5cd
Peter Meißner <retep.meissner@gmail.com>
2023-04-14 11:29:31 +0200

    json - gson

--- src/main/java/de/petermeissner/learnjeerest/PingResource.java
+++ src/main/java/de/petermeissner/learnjeerest/PingResource.java
@@ -1,5 +1,7 @@
 package de.petermeissner.learnjeerest;
 
+import com.google.gson.Gson;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,9 +9,20 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/ping")
 public class PingResource {
+
+    Gson gson = new Gson();
+
+
+    private static class Ping {
+        public String response;
+
+        private Ping() {
+            response = "pong";
+        }
+    }
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public String index() {
-        return "pong";
+        return gson.toJson(new Ping());
     }
 }
